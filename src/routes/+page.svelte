1a537291e0e3573070628cda743eff07673fbccc
Lynix <81735347+cyberlynix@users.noreply.github.com>
2023-09-05 23:34:42 -0300

    Release Partial Update
    The style of this release is inspired by Jae Lo Presti (https://777.tf/)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,36 @@
 <script lang="ts">
 </script>
 
-<h1>
-    Welcome to <span class="text-cyan">lynix</span><span class="text-green">.ca</span>!
-</h1>
-
-<p>
-    Planned Maintenance: Upgrade LynixCA <span class="text-cyan">v1.2.70</span><br/>
-    <span class="text-green">[Coming Soon]</span>
-</p>
-
-<ul>
-    <li>Matrix: @lynix:lynix.ca</li>
-    <li>Discord: @lynix.ca</li>
-    <li>Telegram: @cyberlynix</li>
-</ul>
+<div class="floof-page">
+	<header class="floof-header">
+		<a href="/">
+			<h1>
+				<span class="title">LYNIX.CA</span><br />
+				<span class="subtitle">Cyber Beeps'n Boops</span>
+			</h1>
+		</a>
+	</header>
+	<div class="floof-container">
+		<div class="floof-navigation">
+			<h1>NAVIGATION</h1>
+            <hr/>
+            <p>Copyright 2022-2023 <a href="https://lynix.ca/" class="h-card" rel="me">cyberlynix</a>, this page style is inspired by <a href="https://777.tf/" class="h-card" rel="me">Jae Lo Presti</a>; Powered by <a href="">FloofRender</a> & <a href="">Svelte</a>, under the GNU AGPLv3;</p>
+		</div>
+		<div class="floof-content">
+			<h1>
+                Welcome to <span class="text-cyan">lynix</span><span class="text-green">.ca</span>!
+            </h1>
+            
+            <p>
+                Planned Maintenance: Upgrade LynixCA <span class="text-cyan">v1.2.70</span><br/>
+                <span class="text-green">[Coming Soon]</span>
+            </p>
+            
+            <ul>
+                <li>Matrix: @lynix:lynix.ca</li>
+                <li>Discord: @lynix.ca</li>
+                <li>Telegram: @cyberlynix</li>
+            </ul>
+		</div>
+	</div>
+</div>
