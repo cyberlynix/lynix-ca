b9f88089edd689a9ce6fdf40e9c7fc8eabf78b08
Lynix <81735347+cyberlynix@users.noreply.github.com>
2023-09-06 10:50:15 -0300

    Publish big changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,13 +13,41 @@
 	<div class="floof-container">
 		<div class="floof-navigation">
 			<h1>NAVIGATION</h1>
-            <p style="text-align:center;">No Navigation Links<br/>[Coming Soon]</p>
+            <div class="floof-card">
+                <div class="floof-header">
+                   <h4 style="text-align:center;width:100%;font-size:1.6rem;margin:1rem;">Links</h4>
+                </div>
+                <div class="content">
+                   <p>Pages</p>
+                   <ul>
+                      <li><a href="/">Index</a></li>
+                      <li><a href="https://lynix.ca/blog/">Blog</a></li>
+                   </ul>
+                   <p>Furry Content</p>
+                   <ul>
+                        <li><a href="https://lynix.ca/fursuit/">Fursuit</a></li>
+                       <li><a href="https://lynix.ca/fursona/">Fursona</a></li>
+                    </ul>
+                   <p>Wiki</p>
+                   <ul>
+                      <li><a href="https://lynix.ca/infosec/notes">Cybersecurity Notes</a></li>
+                      <li><a href="https://docs.lynix.ca/">Documentation</a></li>
+                      <li><a href="https://lynix.ca/experiments/">Experiments</a></li>
+                   </ul>
+                </div>
+             </div>
             <hr/>
             <h1>NETWORK STATUS</h1>
-            <p style="text-align:center;color:#dc2626;">Failed to connect to Lynix API!
+            <!--<p style="text-align:center;color:#dc2626;">Failed to connect to Lynix API!
                 <br/>
                 <a href="#retry" style="color:#dc2626;">Retry</a>
-            </p>
+            </p>-->
+            <ul style="list-style: none;font-size:1.4rem;">
+                <li>[<span style="color:#22c55e;">OK</span>] lynix.ca</li>
+                <li>[<span style="color:#22c55e;">OK</span>] api.lynix.ca</li>
+                <li>[<span style="color:#22c55e;">OK</span>] matrix (conduit)</li>
+                <li>[<span style="color:#22c55e;">OK</span>] ns</li>
+            </ul>
             <hr/>
             <p>Copyright 2022-2023 <a href="https://lynix.ca/" class="h-card" rel="me">cyberlynix</a>, this page style is inspired by <a href="https://777.tf/" rel="jae">Jae Lo Presti</a>; Powered by <a href="https://github.com/cyberlynix/floof-render" rel="Floof Render Coming Soon! Built in Rust">FloofRender</a> & <a href="https://svelte.dev/" rel="Svelte Framework: cybernetically enhanced web apps">Svelte</a>, under the GNU AGPLv3;</p>
             <br/>
@@ -36,19 +64,32 @@
             </div>
 
 
-			<h1>
-                Welcome to <span class="text-cyan">lynix</span><span class="text-green">.ca</span>!
-            </h1>
+            <h1>Welcome to my website!</h1>
+            <img src="/images/Lynix_Mulli.png" alt="Lynix's Fursona drawn by Mulliganos" class="sidepfp">
             
             <p>
-                Planned Maintenance: Upgrade LynixCA <span class="text-cyan">v1.2.70</span><br/>
-                <span class="text-green">[Coming Soon]</span>
-            </p>
+                Greetings, I am Anthony, also known as Lynix. I am a dedicated Cybersecurity student actively pursuing opportunities to further my knowledge and gain practical experience in the field. Beyond my academic pursuits, I have a fervor for artistic expression, dedicating my free time to web development. Additionally, I find relaxation and exploration in the realm of video games. I am proudly located in Canada, evident from my domain name using the .ca extension.</p>
             
+            <blockquote>
+                <p><span class="notice" style="font-weight:700;color:#d97706;">[!] Notice:</span> Everything on this website and blog is solely my own work and does not reflect the views of any current or former employers. To dispute any content, please feel free to contact me.</p>
+            </blockquote>
+            
+            <p style="font-weight:bold;">You can reach out to me through the following platforms: </p>
             <ul>
                 <li>Matrix: @lynix:lynix.ca</li>
                 <li>Discord: @lynix.ca</li>
                 <li>Telegram: @cyberlynix</li>
+            </ul>
+            <br/>
+            <hr/>
+            <h2 id="latest-of-the-blog" tabindex="-1">Latest of my blog</h2>
+            <p>Don't miss anything from the blog <a href="/blog/index.xml">by fetching the LynixAPI</a>!<br>
+               Feeds are available in <a href="/blog/index.json">JSON</a>
+            </p>
+            <ul>
+               <li><a href="#">[2023/09/05] A new year of Cybersecurity!</a></li>
+               <li><a href="#">[2023/09/04] Lynix.ca v1.2.72, new styles and changes!</a></li>
+               <li><a href="https://lynix.ca/blog/why-did-i-switch-to-svelte">[2023/08/21] Embracing Svelte: Transforming lynix.ca for Stability</a></li>
             </ul>
 		</div>
 	</div>
