b6ec24fc93ede53dfcae39b9686a3b6ce07cdf94
Lynix <81735347+cyberlynix@users.noreply.github.com>
2023-09-06 00:16:57 -0300

    Update Stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,10 +13,29 @@
 	<div class="floof-container">
 		<div class="floof-navigation">
 			<h1>NAVIGATION</h1>
+            <p style="text-align:center;">No Navigation Links<br/>[Coming Soon]</p>
+            <hr/>
+            <h1>NETWORK STATUS</h1>
+            <p style="text-align:center;color:#dc2626;">Failed to connect to Lynix API!
+                <br/>
+                <a href="#retry" style="color:#dc2626;">Retry</a>
+            </p>
             <hr/>
             <p>Copyright 2022-2023 <a href="https://lynix.ca/" class="h-card" rel="me">cyberlynix</a>, this page style is inspired by <a href="https://777.tf/" class="h-card" rel="me">Jae Lo Presti</a>; Powered by <a href="">FloofRender</a> & <a href="">Svelte</a>, under the GNU AGPLv3;</p>
+            <br/>
+            <p>LynixCA Version: <a href="https://777.tf/" class="h-card" rel="me">v1.2.70</a><br/>
+               FloofRender Version: <a href="https://777.tf/" class="h-card" rel="me">v0.0.3</a></p>
+            <hr>
+
 		</div>
 		<div class="floof-content">
+
+
+            <div class="floof-alert animated-gradient">
+                <p>Welcome to the new Lynix.ca, built in SvelteKit! <span style="font-weight: 600;">Learn More</span></p>
+            </div>
+
+
 			<h1>
                 Welcome to <span class="text-cyan">lynix</span><span class="text-green">.ca</span>!
             </h1>
