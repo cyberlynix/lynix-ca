36b8362c22825999bd531334fd398551d70dcf54
Lynix <81735347+cyberlynix@users.noreply.github.com>
2023-09-07 09:03:39 -0300

    Add temporary handling for 404

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,10 @@ async fn main() {
     let app = Router::new()
         .route("/", get(home))
         .nest_service("/assets", ServeDir::new("assets"))
-        .nest_service("/dist", ServeDir::new("dist"))
+        .fallback(error)
         .with_state(state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
 
     Server::bind(&addr)
         .serve(app.into_make_service())
@@ -35,8 +35,20 @@ struct TemplateData<'a> {
     name: &'a str
 }
 
+#[derive(Serialize)]
+struct ErrorData<'a> {
+    message: &'a str,
+    status: i32
+}
+
 async fn home(State(state): State<AppState<'_>>) -> Html<String> {
     let template = include_str!("../pages/index.hbs");
     let data = TemplateData { name: "Lynix" };
+    Html(state.handlebars.render_template(template, &data).unwrap())
+}
+
+async fn error(State(state): State<AppState<'_>>) -> Html<String> {
+    let template = include_str!("../pages/error.hbs");
+    let data = ErrorData{ message: "Page unavailable, try again later.", status: 404 };
     Html(state.handlebars.render_template(template, &data).unwrap())
 }
